0bda54cb233f73401cdfba5dac3b26a371eb9e8e
Thiago Beltrami Dias Batista <thiago.batista@cxmail.com>
2022-10-21 16:08:00 -0300

    Login - Adds inputTexts for username and login into the LoginView - Thiago Batista

--- BankAccount/LoginView.swift
+++ BankAccount/LoginView.swift
@@ -9,10 +9,15 @@ import UIKit
 
 class LoginView: UIView {
     
+    lazy var loginStackView = UIStackView()
+    lazy var usernameTextField = UITextField()
+    lazy var passwordTextField = UITextField()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         style()
         layout()
+        setViews()
     }
     
     required init?(coder: NSCoder) {
@@ -20,15 +25,82 @@ class LoginView: UIView {
     }
     
     override var intrinsicContentSize: CGSize {
-        return CGSize(width: 200, height:   200) // <<== view size
+        return CGSize(width: 200, height:   100) // <<== view size
     }
     
     //MARK: - View Setup
     private func style() {
-        translatesAutoresizingMaskIntoConstraints = false
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.backgroundColor = .yellow
+        
+        self.addSubview(loginStackView)
+        self.loginStackView.addArrangedSubview(usernameTextField)
+        self.loginStackView.addArrangedSubview(passwordTextField)
+   
+        self.subviews.forEach { $0.translatesAutoresizingMaskIntoConstraints = false }
     }
     
     private func layout() {
         
+    }
+    
+    //MARK: - Elements setup
+    private func setViews() {
+        setLoginStackView()
+        setUsernameInputText()
+        setPasswordInputText()
+    }
+    
+    private func setLoginStackView() {
+        self.loginStackView.axis = .vertical
+        self.loginStackView.spacing = 16
+        
+        NSLayoutConstraint.activate([
+            loginStackView.centerYAnchor.constraint(equalTo: centerYAnchor),
+            loginStackView.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 2),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: loginStackView.trailingAnchor, multiplier: 2)
+        ])
+    }
+    
+    private func setUsernameInputText() {
+        self.usernameTextField.placeholder = "Username"
+        self.usernameTextField.delegate = self
+    }
+    
+    private func setPasswordInputText() {
+        self.passwordTextField.placeholder = "Password"
+        self.passwordTextField.delegate = self
+    }
+}
+
+//Mark: - Extensions
+extension LoginView: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("textFieldShouldReturn")
+        
+  
+        if !textField.hasText {
+            textField.placeholder = "Type here your information"
+            print("Inválid input - cannot be empty")
+            return false } else {
+                textField.resignFirstResponder()
+                print("Valid input - \(textField.text!)")
+            }
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        print("textFieldShouldEndEditing")
+        if textField.text?.count ?? 0 < 5 {
+            print("Inválid input - too short")
+            return false
+        } else {
+            print("Valid input - more than 3 char")
+        }
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("textFieldDidEndEditing")
     }
 }
--- BankAccount/LoginViewController.swift
+++ BankAccount/LoginViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    let loginView = LoginView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        style()
+        layout()
         self.view.backgroundColor = .orange
     }
+    
+    //MARK: - View Setup
+    private func style() {
+        //loginView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func layout() {
+        view.addSubview(loginView)
+        
+        NSLayoutConstraint.activate([
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 2),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 2)
+        ])
+    }
 
-
+    
 }
 
