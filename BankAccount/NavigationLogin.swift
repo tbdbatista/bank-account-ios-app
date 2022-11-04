3e0cf21d5c228c59b53411f26579f6381ba558fd
tbdbatista <tbdbatista@gmail.com>
2022-11-04 15:03:36 -0300

    Home - Sets navigation for others viewControllers embedded into the tabBarController - Thiago Batista

--- BankAccount/NavigationLogin.swift
+++ BankAccount/NavigationLogin.swift
@@ -11,6 +11,19 @@ class NavigationLogin {
     
     static func goHome(presenter: UIViewController) {
         let mainTabBarController = MainTabBarController()
+        mainTabBarController.selectedIndex = 0
+        presenter.goToViewController(navigateTo: mainTabBarController, navigationType: .present, animated: true)
+    }
+    
+    static func goTransactions(presenter: UIViewController) {
+        let mainTabBarController = MainTabBarController()
+        mainTabBarController.selectedIndex = 1
+        presenter.goToViewController(navigateTo: mainTabBarController, navigationType: .present, animated: true)
+    }
+    
+    static func goMore(presenter: UIViewController) {
+        let mainTabBarController = MainTabBarController()
+        mainTabBarController.selectedIndex = 2
         presenter.goToViewController(navigateTo: mainTabBarController, navigationType: .present, animated: true)
     }
     
