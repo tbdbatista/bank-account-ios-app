6989499041b10931db0645bd38ffc950a66feb48
tbdbatista <tbdbatista@gmail.com>
2022-11-04 14:58:38 -0300

    Home - Change tabBar colors - Thiago Batista

--- BankAccount/MainTabBarController.swift
+++ BankAccount/MainTabBarController.swift
@@ -13,23 +13,23 @@ class MainTabBarController: UITabBarController {
     let vc1 = HomeViewController()
     let vc2 = TransactionsViewController()
     let vc3 = MoreInformationViewController()
-    let vc4 = LoginViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         vc1.setTabBarImage(imageName: "list.dash.header.rectangle", title: "Summary")
         vc2.setTabBarImage(imageName: "arrow.left.arrow.right", title: "Transactions")
-        vc3.setTabBarImage(imageName: "ellipsis.circle", title: "More")
-        vc4.setTabBarImage(imageName: "arrow.left.arrow.right", title: "Teste")
+        vc3.setTabBarImage(imageName: "ellipsis", title: "More")
+        
+        self.tabBar.tintColor = .primaryGreen
+        self.tabBar.unselectedItemTintColor = .gray
         
         let nc1 = UINavigationController(rootViewController: vc1)
         let nc2 = UINavigationController(rootViewController: vc2)
         let nc3 = UINavigationController(rootViewController: vc3)
-        let nc4 = UINavigationController(rootViewController: vc4)
         
         nc1.setStatusBar()
         
-        self.viewControllers = [nc1, nc2, nc3, nc4]
+        self.viewControllers = [nc1, nc2, nc3]
     }
 }
