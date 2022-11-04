6f07d3952c9245ddb706ed3c2a7c9f3d89392a16
tbdbatista <tbdbatista@gmail.com>
2022-11-04 14:09:32 -0300

    Organizing project files

--- BankAccount/ViewControllerExtension.swift
+++ BankAccount/ViewControllerExtension.swift
@@ -68,8 +68,7 @@ extension UIViewController {
     }
     
     func setTabBarImage(imageName: String, title: String) {
-        let configuration = UIImage.SymbolConfiguration(scale: .large)
-        let image = UIImage(systemName: imageName, withConfiguration: configuration)
+        let image = UIImage(systemName: imageName, withConfiguration: .none)
         self.tabBarItem = UITabBarItem(title: title, image: image, tag: 0)
     }
 }
