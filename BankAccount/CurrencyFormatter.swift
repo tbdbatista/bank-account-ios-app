7f82a660ed0fe4ff0cbb9cde47b90919ce7394ae
tbdbatista <tbdbatista@gmail.com>
2022-11-18 15:55:47 -0300

    Minor changes

--- BankAccount/CurrencyFormatter.swift
+++ BankAccount/CurrencyFormatter.swift
@@ -12,21 +12,18 @@ class CurrencyFormatter {
     func formatDollar(dollarsPart: Double) -> String {
         let dollarsInt = Int(dollarsPart)
         var dollarsString = String(dollarsInt)
-        
         var dollarsReversedString = String(dollarsString.reversed())
-        var dollarsReversedWithCommasString = dollarsReversedString
-        
         var commasQuantity = 0
+        
         for n in 0...dollarsReversedString.count - 1 {
+            // checks if n is first index, and if n contains "-" as in a negative number
             if n%3 == 0, n != 0, dollarsReversedString[dollarsReversedString.index(dollarsReversedString.startIndex, offsetBy: n + commasQuantity)] != "-" {
                 let i = dollarsReversedString.index(dollarsReversedString.startIndex, offsetBy: n + commasQuantity)
                 dollarsReversedString.insert(",", at: i)
                 commasQuantity += 1
             }
         }
-        
         dollarsString = String(dollarsReversedString.reversed())
-        
         return dollarsString
     }
 }
