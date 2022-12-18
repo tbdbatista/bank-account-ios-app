029815e70af3ab4059dd2983b64dae85a8ba7192
tbdbatista <tbdbatista@gmail.com>
2022-12-18 15:57:05 -0300

    Cleaning - Removes hard code values used before implementing networking to fetch data - Thiago Batista

--- BankAccount/HomeViewModel.swift
+++ BankAccount/HomeViewModel.swift
@@ -18,17 +18,7 @@ class HomeViewModel {
 
     private let fetchAccountProfileUseCase = FetchAccountProfileUseCase()
     private let fetchAccountDetailsUseCase = FetchAccountDetailsUseCase()
-    
-    func fetchAccountsData() -> [HomeAccountModel] {
-        let account = HomeAccountModel(accountType: .Banking, accountName: "Single Account", dollars: "90,909", cents:"76")
-        let savings = HomeAccountModel(accountType: .Banking, accountName: "Savings", dollars: "123,423", cents:"90")
-        let visa = HomeAccountModel(accountType: .CreditCard, accountName: "Visa", dollars: "14,414", cents:"41")
-        let investment = HomeAccountModel(accountType: .Investment, accountName: "REIT Investiments Annually", dollars: "980,745,789", cents:"55")
-        let crypto = HomeAccountModel(accountType: .Cryptocurrency, accountName: "Bitcoin", dollars: "234,423", cents:"32")
-        let accounts = [account, savings, visa, investment, crypto]
-        return accounts
-    }
-    
+  
     func getAccountsDetails(completion: @escaping ([AccountDetailsResponse]?, String?) -> Void) {
         fetchAccountDetailsUseCase.fetchAccountDetails(id: "1") { result in
             switch result {
